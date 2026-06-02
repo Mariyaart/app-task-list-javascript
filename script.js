392f6fa751e2a45717b7bbe35606edd6ec19032d
Mariyaart <ovdiichuk_ak24@nuwm.edu.ua>
2026-06-02 12:56:47 +0300

    ramdom commit

--- script.js
+++ script.js
@@ -1,56 +1,16 @@
-const taskInput = document.getElementById("task-input");
-const taskAddBtn = document.getElementById("task-add-btn");
-const taskList = document.getElementById("task-list");
+'use strict';
 
-document.addEventListener("DOMContentLoaded", loadTasks);
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click',function () { 
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
 
-function addTask(text = null, completed = false) {
-    const value = text || taskInput.value.trim();
-    if (value === "") return;
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
 
-    const task = document.createElement("div");
-    task.className = "task";
-
-    task.innerHTML = `
-        <input type="checkbox" ${completed ? "checked" : ""}>
-        <span class="task-text" contenteditable="true">${value}</span>
-        <button class="delete">✖</button>
-    `;
-
-    task.querySelector(".delete").onclick = () => {
-        task.remove();
-        saveTasks();
-    };
-
-    task.querySelector("input").onchange = saveTasks;
-    task.querySelector(".task-text").oninput = saveTasks;
-
-    taskList.appendChild(task);
-
-    taskInput.value = "";
-    saveTasks();
-}
-
-taskAddBtn.onclick = () => addTask();
-
-taskInput.addEventListener("keypress", e => {
-    if (e.key === "Enter") addTask();
+    console.log('current class name: ' + className);
 });
-
-function saveTasks() {
-    const tasks = [];
-
-    document.querySelectorAll(".task").forEach(t => {
-        tasks.push({
-            text: t.querySelector(".task-text").innerText,
-            completed: t.querySelector("input").checked
-        });
-    });
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function loadTasks() {
-    const data = JSON.parse(localStorage.getItem("tasks")) || [];
-    data.forEach(t => addTask(t.text, t.completed));
-}
